64918b6b21b5ccc3683e44c847081c5d68484f81
Vivek Patel <vivek_patel@umail.ucsb.edu>
2016-01-22 00:29:41 -0800

    Implemented basic divide-and-conquer algorithm successfully

--- closestPair.cpp
+++ closestPair.cpp
@@ -1,3 +1,5 @@
+using namespace std;
+
 #include <cstdio>
 #include <cstring>
 #include <string>
@@ -5,14 +7,17 @@
 #include <vector>
 #include <cmath>
 #include <climits>
-
-using namespace std;
+#include <limits>
+#include <algorithm>
+#include <float.h>
 
 bool pairCompareX(const pair<double, double>& firstElem, const pair<double, double>& secondElem);
 bool pairCompareY(const pair<double, double>& firstElem, const pair<double, double>& secondElem);
 double distance(pair<double, double> firstPair, pair<double, double> secondPair);
 pair<double, double>* brute(vector<pair<double, double>> points, int n);
 pair<double, double>* basic(vector<pair<double, double>> points, int n);
+pair<double, double>* basicHelper(vector<pair<double, double>> points, int n);
+pair<double, double>* minimumStrip(pair<double, double>* strip, int size, double delta);
 pair<double, double>* optimal(vector<pair<double, double>> points, int n);
 
 int main(int argc, char** argv)
@@ -28,22 +33,25 @@ int main(int argc, char** argv)
 				points.push_back(tempPair);
 			}
 			printf("%d\n", points.size());
-			pair<double, double>* targetPoints;
+			pair<double, double>* targetPoints = new pair<double, double>[2];
 			if (strcmp(arg, "brute") == 0) {
 				printf("Running brute force algorithm...\n");
 				targetPoints = brute(points, points.size());
+				printf("Distance: %lf\n", distance(targetPoints[0], targetPoints[1]));
 				for (int i = 0; i < 2; i++) {
 					printf("(%.7lf, %.7lf)\n", targetPoints[i].first, targetPoints[i].second);
 				}
 			} else if (strcmp(arg, "basic") == 0) {
 				printf("Running basic algorithm...\n");
 				targetPoints = basic(points, points.size());
+				printf("Distance: %lf\n", distance(targetPoints[0], targetPoints[1]));
 				for (int i = 0; i < 2; i++) {
 					printf("(%.7lf, %.7lf)\n", targetPoints[i].first, targetPoints[i].second);
 				}
 			} else if (strcmp(arg, "optimal") == 0) {
 				printf("Running optimal algorithm...\n");
 				targetPoints = optimal(points, points.size());
+				printf("Distance: %lf\n", distance(targetPoints[0], targetPoints[1]));
 				for (int i = 0; i < 2; i++) {
 					printf("(%.7lf, %.7lf)\n", targetPoints[i].first, targetPoints[i].second);
 				}
@@ -69,7 +77,7 @@ double distance(pair<double, double> firstPair, pair<double, double> secondPair)
 
 pair<double, double>* brute(vector<pair<double, double>> points, int n)
 {
-	double minDist = INT_MAX;
+	double minDist = DBL_MAX;
 	pair<double, double>* targetPoints = new pair<double, double>[2];
 	for (int i = 0; i < n; i++) {
 	   for (int j = i+1; j < n; j++) {
@@ -80,14 +88,13 @@ pair<double, double>* brute(vector<pair<double, double>> points, int n)
 			}
 		}
 	}
-	printf("Distance: %lf\n", minDist);
 	return targetPoints;
 }
 
 pair<double, double>* basic(vector<pair<double, double>> points, int n) {
 	pair<double, double>* targetPoints = new pair<double, double>[2];
-	
-	return targetPoints;
+	sort(points.begin(), points.end(), pairCompareX);
+	return basicHelper(points, points.size());
 }
 
 pair<double, double>* basicHelper(vector<pair<double, double>> points, int n) {
@@ -99,18 +106,28 @@ pair<double, double>* basicHelper(vector<pair<double, double>> points, int n) {
     int mid = n/2;
     pair<double, double> median = points[mid];
 
-	pair<double, double>* resultRight = new pair<double, double>[2];
+	pair<double, double>* resultRight = new pair<double, double>[2]; 
 	pair<double, double>* resultLeft = new pair<double, double>[2];
+	pair<double, double>* result = new pair<double, double>[2];
 
     resultRight = basicHelper(points, mid);
-    resultLeft = basicHelper(points + mid, n - mid);
+    resultLeft = basicHelper(points, n - mid);
 
 	double deltaRight = distance(resultRight[0], resultRight[1]);
 	double deltaLeft = distance(resultLeft[0], resultLeft[1]);
+	double delta;
  
-    double delta = min(deltaRight, deltaLeft);
+    if (deltaRight <= deltaLeft) {
+    	result = resultRight;
+    	delta = deltaRight;
+    }
+    else {
+    	result = resultLeft;
+    	delta = deltaLeft;
+    }
  
-    pair<double, double>* strip = new pair<double, double>[n];
+    pair<double, double> strip[n];
+    pair<double, double>* stripResult = new pair<double, double>[2];
 
     int index = 0;
     for (int i = 0; i < n; i++) {
@@ -120,24 +137,35 @@ pair<double, double>* basicHelper(vector<pair<double, double>> points, int n) {
 		}
 	} 
 
-    return min(delta, stripClosest(strip, j, delta));
+	stripResult = minimumStrip(strip, index, delta);
+	double stripDist = distance(stripResult[0], stripResult[1]);
+
+	if (delta <= stripDist) {
+		return result;
+	}
+	else {
+		return stripResult;
+	}
 }
 
-double stripClosest(pair<double, double>* strip, int size, float d)
+pair<double, double>* minimumStrip(pair<double, double>* strip, int size, double delta)
 {
-    double min = delta;
+    double minimum = delta;
+ 	pair<double, double>* result = new pair<double, double>[2];
+ 	result[0] = make_pair(-DBL_MAX, -DBL_MAX);
+ 	result[1] = make_pair(DBL_MAX, DBL_MAX);
+    sort(strip, strip + size, pairCompareY); 
+    for (int i = 0; i < size; i++) {
+        for (int j = i+1; j < size && (strip[j].second - strip[i].second) < minimum; j++) {
+            if (distance(strip[i],strip[j]) < minimum) {
+                minimum = distance(strip[i], strip[j]);
+                result[0] = strip[i];
+                result[1] = strip[j];
+            }
+        }
+    }
  
-    sort(strip, size, sizeof(Point), compareY); 
- 
-    // Pick all points one by one and try the next points till the difference
-    // between y coordinates is smaller than d.
-    // This is a proven fact that this loop runs at most 6 times
-    for (int i = 0; i < size; ++i)
-        for (int j = i+1; j < size && (strip[j].y - strip[i].y) < min; ++j)
-            if (dist(strip[i],strip[j]) < min)
-                min = dist(strip[i], strip[j]);
- 
-    return min;
+    return result;
 }
 
 pair<double, double>* optimal(vector<pair<double, double>> points, int n) {
